1c5de2088aecd58e97e2c09ce87eaa673c469877
Nando473L <ferc3455@gmail.com>
2024-03-14 20:18:34 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter WIDGETS'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple, // Cambia el color de la barra de aplicaciones a deepPurple
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            
+            // Espacio adicional
+            SizedBox(height: 20),
+
+            // Nuevo widget Row
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Icon(
+                  Icons.favorite,
+                  color: Colors.pink,
+                  size: 24.0,
+                  semanticLabel: 'Text to announce in accessibility modes',
+                ),
+                Icon(
+                  Icons.audiotrack,
+                  color: Colors.green,
+                  size: 30.0,
+                ),
+                Icon(
+                  Icons.beach_access,
+                  color: Colors.blue,
+                  size: 36.0,
+                ),
+              ],
+            ),
+            SizedBox(height: 20),
+            Center(
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              color: Colors.amber[600],
+              width: 48.0,
+              height: 48.0,
+            ), 
+          ),
+           SizedBox(height: 60),
+           Container(
+            width: 125.0,
+            height: 60.0,
+            color: Colors.blue,
+            child: Text('Widget de Diseño 1 Widget de Diseño 1 Widget de Diseño 1'),
+          ),
+          SizedBox(height: 20),
+            Column(
+              children: [
+                Text('Widget de Diseño 2'),
+                Text('Widget de Diseño 2'),
+                Text('Widget de Diseño 2'),
+              ],
+            ),
+          SizedBox(height: 20),
+          
+                Text(
+                  'Widget de Diseño 3',
+                  style: TextStyle(color:Color.fromARGB(255, 122, 18, 117),
+                  fontWeight: FontWeight.bold),
+                ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
